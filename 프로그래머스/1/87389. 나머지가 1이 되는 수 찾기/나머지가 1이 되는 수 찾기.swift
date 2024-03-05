6d79a70afd9acfe092b8777609da2b01c1a4ed2e
Calia <144116848+eqqmayo@users.noreply.github.com>
2024-03-05 17:53:16 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.swift
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.swift
@@ -1,9 +1,10 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var x = 2
-    while n % x != 1 {
-        x += 1
+    for x in 1...n {
+        if n % x == 1 {
+            return x
+        }
     }
-    return x
+    return 0
 }
