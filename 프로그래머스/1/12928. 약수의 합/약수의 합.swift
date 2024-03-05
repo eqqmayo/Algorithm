2c67e6cfbe8656941a12a77cb911a351b532c2fe
Calia <144116848+eqqmayo@users.noreply.github.com>
2024-03-05 17:40:33 +0900

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.swift
@@ -0,0 +1,11 @@
+func solution(_ n:Int) -> Int {
+    var result = 0
+    guard n > 0 else { return 0}
+    
+    for i in 1...n {
+        if n % i == 0 {
+            result += i
+        }
+    }
+    return result
+}
