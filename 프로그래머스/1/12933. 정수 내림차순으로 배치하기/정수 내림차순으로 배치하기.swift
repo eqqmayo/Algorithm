8784b9fdcbd0ac81f9de905fbcadfcd846cdffc4
Calia <144116848+eqqmayo@users.noreply.github.com>
2024-03-06 09:55:04 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.13 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.swift
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 func solution(_ n:Int64) -> Int64 {
-    var str = ""
-    let arr = String(n).map { String($0) }.sorted(by: >)
-    for i in arr {
-        str += i
-    }
-    return Int64(str)!
+    return Int64(String(String(n).sorted(by: >)))!
 }
