44a3a57332bc844daacb98d4db349218c846afa8
Calia <144116848+eqqmayo@users.noreply.github.com>
2024-03-08 00:22:31 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.11 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
@@ -1,5 +1,5 @@
 func solution(_ arr:[Int]) -> [Int] {
-    if arr.count == 1 { return [-1] }
-    let min = arr.min()!
-    return arr.filter { $0 > min }
+    var ans = arr
+    ans.remove(at: arr.firstIndex(of: arr.min()!)!)
+    return ans.isEmpty ? [-1] : ans
 }
