c160c7a6670d2764fc10aa322483ff4e344ac7de
Calia <144116848+eqqmayo@users.noreply.github.com>
2024-03-14 13:14:17 +0900

    [level 1] Title: 시저 암호, Time: 2.27 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.swift
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.swift
@@ -2,7 +2,7 @@ func solution(_ s:String, _ n: Int) -> String {
     var asciiArr = s.utf8.map { Int($0) }
     var result = ""
     for i in 0..<asciiArr.count {
-        switch asciiArr[i] {
+        switch i {
         case 65...90:
             asciiArr[i] = (asciiArr[i] + n - 65) % 26 + 65
         case 97...122:
