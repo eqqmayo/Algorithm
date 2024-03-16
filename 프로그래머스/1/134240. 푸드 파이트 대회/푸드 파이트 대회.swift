399fa56c7aa0bea62b60a32902e87a1c0913741a
Calia <144116848+eqqmayo@users.noreply.github.com>
2024-03-16 17:10:10 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.swift
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.swift
@@ -4,8 +4,7 @@ func solution(_ food:[Int]) -> String {
     var str = ""
     var num = 1
     for i in 1..<food.count {
-        str += String(repeating: String(num), count: food[i] / 2)
-        num += 1
+        str += String(repeating: String(i), count: food[i] / 2)
     }
     return str + "0" + str.reversed()
 }
