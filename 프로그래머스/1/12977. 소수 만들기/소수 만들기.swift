06da2570575267615960bd0c6df2c2b12b0c6037
Calia <144116848+eqqmayo@users.noreply.github.com>
2024-03-17 11:23:28 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func isPrime(_ n: Int) -> Bool {
+    if n < 3 { return false }
+    for i in 2..<n {
+        if n % i == 0 { return false }
+    }
+    return true
+}
+
+func solution(_ nums:[Int]) -> Int {
+    var answer = 0
+    for i in 0..<nums.count-2 {
+        for j in i+1..<nums.count-1 {
+            for k in j+1..<nums.count {
+                if isPrime(nums[i] + nums[j] + nums[k]) { answer += 1 }
+            }
+        }
+    }
+    return answer
+}
