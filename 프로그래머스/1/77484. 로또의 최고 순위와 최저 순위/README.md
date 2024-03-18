8ad63ef8d868787fe55aa6059bb134bcee22c0de
Calia <144116848+eqqmayo@users.noreply.github.com>
2024-03-18 18:12:08 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.2 MB, 시간: 0.04 ms
+메모리: 16.5 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 18일 18:04:03
+2024년 03월 18일 18:12:06
 
 ### 문제 설명
 
--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
@@ -1,13 +1,8 @@
 import Foundation
 
 func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
-    var zero = 0
-    var min = 0
-    let rank = [0: 6, 1: 6, 2: 5, 3: 4, 4: 3, 5: 2, 6: 1]
-    for num in lottos {
-        if win_nums.contains(num) { min += 1 }
-        if num == 0 { zero += 1 }
-    }
-    var max = min + zero
-    return [rank[max]!, rank[min]!]
+    var zero = lottos.filter { $0 == 0 }.count
+    var correct = lottos.filter { win_nums.contains($0) }.count
+
+    return [min(7-(correct+zero), 6), min(7-correct, 6)]
 }
