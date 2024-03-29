85ed7779b52073221921b56227af6b5169409ca2
Calia <144116848+eqqmayo@users.noreply.github.com>
2024-03-29 17:02:20 +0900

    [level 1] Title: 문자열 나누기, Time: 2.18 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/README.md
+++ 프로그래머스/1/140108. 문자열 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 나누기 - 140108 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 3.84 ms
+메모리: 16.2 MB, 시간: 2.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 29일 16:36:25
+2024년 03월 29일 17:02:18
 
 ### 문제 설명
 
--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
@@ -1,26 +1,19 @@
 import Foundation
 
 func solution(_ s:String) -> Int {
-    var s = s.map { $0 }
-    var arr = [Character]()
+    var x: Character? = nil
     var count = 0
     var answer = 0
     
-    for i in 0..<s.count {
-        if arr == [] || arr.first! == s[i] {
+    for i in s {
+        if x == nil {
+            x = i
             count += 1
-            arr.append(s[i])
-        } else {
-            count -= 1
-            arr.append(s[i])
-        }
-        if arr != [] && count == 0 {
-            arr = []
             answer += 1
+            continue
         }
-        if count > 0 && i == s.count - 1 {
-            answer += 1
-        }
+        count += x == i ? 1 : -1
+        if count == 0 { x = nil }
     }
     return answer
 }
