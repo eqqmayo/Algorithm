6e7a52a8bb8cb1335e3e29804d1953113ae2b1e4
Calia <144116848+eqqmayo@users.noreply.github.com>
2024-03-31 00:41:21 +0900

    [level 1] Title: 공원 산책, Time: 0.32 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.swift
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.swift
@@ -1,72 +1,44 @@
 import Foundation
 
 func solution(_ park:[String], _ routes:[String]) -> [Int] {
-    var route = [[String]]()
-    var start = [0,0]
-    var dict = ["E": 1, "S": 1, "W": -1, "N": -1]
+    var (x, y) = (0, 0)
+    var blocked: [[Int]] = []
     
-    for i in 0..<park.count {          
-        let arr = Array(park[i]).map{String($0)}
-        if arr.contains("S") {
-            start[0] = i
-            for j in 0..<arr.count {
-                if arr[j] == "S" {
-                    start[1] = j
-                }
-            }
+    for (i, str) in park.enumerated() {
+        for (j, char) in str.enumerated() {
+            if char == "S" { (x, y) = (i, j) }
+            if char == "X" { blocked.append([i, j]) }
         }
-        route.append(arr)
     }
     
-    for i in 0..<routes.count {       
-        var arr = routes[i].split(separator: " ").map{ String($0)}
-        var move = dict[arr[0]]! * Int(arr[1])!
-        let rowIdx = route[0].count - 1
-        let colIdx = route.count - 1
-        var check = [String]()
+    for route in routes {
+        let a = route.split(separator: " ")
+        let (direction, n) = (a[0], Int(a[1])!)
         
-        if arr[0] == "E" || arr[0] == "W" {
-            var moved = start[1] + move
-            if moved < 0 || moved > rowIdx {    
-                continue
-            }
-            if arr[0] == "E" {             
-                for j in start[1]...moved {
-                check.append(route[start[0]][j])
-                }
-            } else {
-                for j in moved...start[1] {
-                check.append(route[start[0]][j])
-                }
-            }
-            if check.contains("X") {           
-                continue
-            } else {
-                start = [start[0], moved]
-            }
+        switch direction {
+        case "N":
+            if x - n < 0 || (1...n).contains(where: { i in
+                blocked.contains([x - i, y])
+            }) { break }
+            x -= n
+        case "S":
+            if x + n > park.count - 1 || (1...n).contains(where: { i in
+                blocked.contains([x + i, y])
+            }) { break }
+            x += n
+        case "E":
+            if y + n > park[0].count - 1 || (1...n).contains(where: { i in
+                blocked.contains([x, y + i])
+            }) { break }
+            y += n
+        case "W":
+            if y - n < 0 || (1...n).contains(where: { i in
+                blocked.contains([x, y - i])
+            }) { break }
+            y -= n
+        default:
+            break
         }
-        
-        if arr[0] == "S" || arr[0] == "N" {
-            var moved = start[0] + move
-            if moved < 0 || moved > colIdx {    
-                continue
-            }
-            if arr[0] == "S" {                
-                for j in start[0]...moved {
-                check.append(route[j][start[1]])
-                }
-            } else {
-                for j in moved...start[0] {
-                check.append(route[j][start[1]])
-                }
-            }
-            if check.contains("X") {            
-                continue
-            } else {
-                start = [moved, start[1]]
-            }
-        }
-        
     }
-    return start
+    return [x, y]
 }
